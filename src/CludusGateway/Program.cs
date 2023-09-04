9f2f213e2b0199d7431eca46ac1a5ee020fc9e1b
Alejandro <acksecurity@hotmail.com>
2023-09-04 17:27:29 -0400

    Add websocker support

---
+++ src/CludusGateway/Program.cs
@@ -0,0 +1,40 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// <snippet_UseWebSockets>
+var webSocketOptions = new WebSocketOptions
+{
+    KeepAliveInterval = TimeSpan.FromMinutes(2)
+};
+
+app.UseWebSockets(webSocketOptions);
+// </snippet_UseWebSockets>
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/ws")
+    {
+        if (context.WebSockets.IsWebSocketRequest)
+        {
+            using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+            await CludusGateway.Helpers.EchoHelper.Echo(webSocket);
+        }
+        else
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        }
+    }
+    else
+    {
+        await next(context);
+    }
+
+});
+
+app.Run();
